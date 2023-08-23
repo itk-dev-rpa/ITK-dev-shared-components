f4e9a6bb05a09c86a4262b23f807bad9e8076473
Mathias G <ghbm@aarhus.dk>
2023-08-23 14:33:57 +0200

    Merged similar modules

--- ITK_dev_shared_components/SAP/sap_login.py
+++ ITK_dev_shared_components/SAP/sap_login.py
@@ -1,13 +1,14 @@
-"""This module provides a single function to open SAP GUI using the KMD Portal website."""
+"""This module provides a functions to open SAP GUI."""
 
 import os
 import pathlib
+import subprocess
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 
-def login(user:str, password:str):
+def login_using_portal(username:str, password:str):
     """Open KMD Portal in Edge, login and start SAP GUI.
 
     Args:
@@ -25,7 +26,7 @@ def login(user:str, password:str):
     login_button = driver.find_element(By.ID, 'buttonLogon')
 
     user_field.clear()
-    user_field.send_keys(user)
+    user_field.send_keys(username)
 
     pass_field.clear()
     pass_field.send_keys(password)
@@ -43,8 +44,9 @@ def login(user:str, password:str):
 
     #TODO: Wait for if SAP has opened
 
+
 def _wait_for_download():
-    """Private function that checks if the SAP .erp file has been downloaded.
+    """Private function that checks if the SAP.erp file has been downloaded.
 
     Raises:
         TimeoutError: If the file hasn't been downloaded within 5 seconds.
@@ -60,8 +62,32 @@ def _wait_for_download():
         time.sleep(0.5)
     raise TimeoutError(f".SAP file not found in {downloads_folder}")
 
+
+def login_using_cli(username: str, password: str, client='751', system='P02') -> None:
+    """Open and login to SAP with commandline expressions.
+
+    Args:
+        username (str): AZ username
+        password (str): password
+        client (str, optional): Kommune ID (Aarhus = 751). Defaults to '751'.
+        system (str, optional): Environment SID (e.g. P02 = 'KMD OPUS Produktion [P02]'). Defaults to 'P02'.
+    """    
+    
+    command_args = [
+        r"C:\Program Files (x86)\SAP\FrontEnd\SAPgui\sapshcut.exe",
+        f"-system={system}",
+        f"-client={client}",
+        f"-user={username}",
+        f"-pw={password}"
+    ]
+
+    subprocess.run(command_args, check=False)
+
+
 if __name__=="__main__":
-    user, password = os.environ['Opus Login'].split(';')
-    login(user, password)
+    user = "az12345"
+    password = "Hunter2"
+    login_using_portal(user, password)
+    login_using_cli(user, password)
 
 
