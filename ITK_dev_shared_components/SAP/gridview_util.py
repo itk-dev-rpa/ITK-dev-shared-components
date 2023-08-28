f6a1a937fb419e1b5785f4df844daa807d7ba97a
Mathias G <ghbm@aarhus.dk>
2023-08-28 13:45:27 +0200

    Docstring updated to google format

--- ITK_dev_shared_components/SAP/gridview_util.py
+++ ITK_dev_shared_components/SAP/gridview_util.py
@@ -2,9 +2,15 @@
 
 def scroll_entire_table(grid_view, return_to_top=False) -> None:
     """This function scrolls through the entire table to load all cells.
-    grid_view: A SAP GuiGridView object.
-    return_to_top: Whether to return the table to the first row after scrolling.
-    """
+
+    Args:
+        grid_view: A SAP GuiGridView object.
+        return_to_top: Whether to return the table to the first row after scrolling. Defaults to False.
+
+    Returns:
+        _type_: _description_
+    """    
+
     for i in range(0, grid_view.RowCount, grid_view.VisibleRowCount):
         grid_view.FirstVisibleRow = i
     
@@ -14,10 +20,16 @@ def scroll_entire_table(grid_view, return_to_top=False) -> None:
 
 def get_all_rows(grid_view, pre_load=True) -> tuple[tuple[str]]:
     """Get all values from a table and return them in a 2D tuple.
-    grid_view: A SAP GuiGridView object.
-    pre_load: Whether to first scroll through the table to load all values.
-              If a row hasn't been loaded before reading, the row data will be empty.
-    """
+
+    Args:
+        grid_view: A SAP GuiGridView object.
+        pre_load: Whether to first scroll through the table to load all values.
+                  If a row hasn't been loaded before reading, the row data will be empty. 
+
+    Returns:
+        tuple[tuple[str]]: A 2D tuple of all cell values in the gridview.
+    """    
+
     if pre_load:
         scroll_entire_table(grid_view, True)
 
@@ -37,13 +49,19 @@ def get_all_rows(grid_view, pre_load=True) -> tuple[tuple[str]]:
     return tuple(output)
 
 
-def get_row(grid_view, row, scroll_to_row=False) -> tuple[str]:
-    """Return the data of a single row
-    grid_view: A SAP GuiGridView object.
-    row: The zero-based index of the row.
-    scroll_to_row: Whether to scroll to the row before reading the data.
-                   This ensures the data of the row has been loaded before reading.
-    """
+def get_row(grid_view, row:int, scroll_to_row=False) -> tuple[str]:
+    """Returns the data of a single row.
+
+    Args:
+        grid_view: A SAP GuiGridView object.
+        row: The zero-based index of the row.
+        scroll_to_row: Whether to scroll to the row before reading the data.
+                       This ensures the data of the row has been loaded before reading. 
+
+    Returns:
+        tuple[str]: A tuple of the row's data.
+    """    
+
     if scroll_to_row:
         grid_view.FirstVisibleRow = row
 
@@ -59,8 +77,14 @@ def iterate_rows(grid_view) -> tuple[str]:
     This is preferable to loading all rows as this will only load a row when it's needed.
     As a side effect this function scrolls the yielded row into view, so the
     table doesn't need to be pre-loaded before calling this function.
-    grid_view: A SAP GuiGridView object.
-    """
+
+    Args:
+        grid_view: A SAP GuiGridView object.
+
+    Yields:
+        tuple[str]: A tuple of the next row's data.
+    """    
+
     row = 0
     while row < grid_view.RowCount:
         # Only scroll when row isn't visible
@@ -73,18 +97,34 @@ def iterate_rows(grid_view) -> tuple[str]:
 
 def get_column_titles(grid_view) -> tuple[str]:
     """Get the column titles of the table instead of the column ids.
-    grid_view: A SAP GuiGridView object.
-    """
+    To get the column ids use grid_view.ColumnOrder.
+
+    Args:
+        grid_view: A SAP GuiGridView object.
+
+    Returns:
+        tuple[str]: A tuple of the gridview's column titles.
+    """    
+
     return tuple(grid_view.GetColumnTitles(c)[0] for c in grid_view.ColumnOrder)
 
 
 def find_row_index_by_value(grid_view, column:str, value:str) -> int:
     """Find the index of the first row where the given column's value
-    match the given value. Returns -1 if no row is found.
-    grid_view: A SAP GuiGridView object.
-    column: The name of the column whose value to check.
-    value: The value to search for.
-    """
+    matches the given value. Returns -1 if no row is found.
+
+    Args:
+        grid_view: A SAP GuiGridView object.
+        column: The id of the column.
+        value: The value to search for.
+
+    Raises:
+        ValueError: If the gridview doesn't contain a column with the given id.
+
+    Returns:
+        int: The index of the first row which column value matches the given value.
+    """    
+
     if column not in grid_view.ColumnOrder:
         raise ValueError(f"Column '{column}' not in grid_view")
 
