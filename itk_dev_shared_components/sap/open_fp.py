22f64a968759bfea20fb2d752ece124ff0a112ec
Anders Krogsager <andkr@aarhus.dk>
2023-12-06 15:09:29 +0100

    module for opening Forretnignspartneroversigt in SAP

---
+++ itk_dev_shared_components/sap/open_fp.py
@@ -0,0 +1,33 @@
+"""Method for opening Forretnignspartneroversigt in SAP"""
+
+def find_forretningspartner(session, fp) -> None:
+    """Start a transaction FMCACOV and open the fp. Dismiss pop-up where user must select CPR or CVR.
+    The process will open Forretningspartneroversigt for the given fp.
+    If the fp is invalid
+
+    Args:
+        session: The SAP session to preform the action.
+        fp: The forretningspartner number.
+
+    Raises:
+        ValueError: If the forretningspartner was not found.
+    """
+    session.StartTransaction('fmcacov')
+
+    # Find forretningspartner
+    session.findById("wnd[0]/usr/ctxtGPART_DYN").text = fp
+    session.findById("wnd[0]").sendVKey(0)
+    # detect window "Forretningspartner * Entries"
+
+    if session.findById('wnd[1]/usr', False) is not None:
+        # pop-up detected
+        for row_id in range(3, 5):
+            fp_row = session.FindById(f'wnd[1]/usr/lbl[103,{row_id}]')
+            if fp_row.text == fp:
+                fp_row.SetFocus()
+                # press 'Accept' button.
+                session.FindById('wnd[1]/tbar[0]/btn[0]').press()
+                break
+        else:
+            # range exhausted
+            raise ValueError(f"ForretnPartner '{fp}' was not found in pop-up.")
---
+++ tests/test_sap/test_open_fp.py
@@ -0,0 +1,23 @@
+"""Integration test of opening fp in SAP"""
+import unittest
+import os
+from itk_dev_shared_components.sap import sap_login, open_fp, multi_session
+
+class IntegratonTestFp(unittest.TestCase):
+    """Integration test of opening fp in SAP"""
+    def setUp(self):
+        sap_login.kill_sap()
+        user, password = os.environ['SAP Login'].split(';')
+        sap_login.login_using_cli(user, password)
+
+    def tearDown(self):
+        sap_login.kill_sap()
+
+    def test_open_fp(self):
+        """Test opening Forretnignspartneroversigt in SAP"""
+        session = multi_session.get_all_sap_sessions()[0]
+        test_fp = ""
+        open_fp.find_forretningspartner(session,test_fp)
+
+if __name__ == '__main__':
+    unittest.main()
