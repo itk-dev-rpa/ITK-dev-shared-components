a708b20995135289b096602e51bdad66fadef6f2
Mathias G <ghbm@aarhus.dk>
2024-01-30 13:35:22 +0100

    Added Task and type hints

--- itk_dev_shared_components/kmd_nova/nova_objects.py
+++ itk_dev_shared_components/kmd_nova/nova_objects.py
@@ -1,6 +1,8 @@
 """This module contains dataclasses relating to KMD Nova cases."""
 
 from dataclasses import dataclass
+from typing import Literal, Optional
+from datetime import datetime
 
 # All classes in this module are pure dataclasses made to contain data.
 # pylint: disable=too-many-instance-attributes
@@ -19,14 +21,14 @@ class CaseParty:
 class Document:
     """A dataclass representing a KMD Nova Document."""
     uuid: str
-    document_number: str
     title: str
-    sensitivity: str
-    document_type: str
+    sensitivity: Literal["Fortrolige", "IkkeFortrolige", "SærligFølsomme", "Følsomme"]
+    document_type: Literal["Indgående", "Udgående", "Internt", "Dagsordenpunkt", "Dagsorden", "Referat", "Andet", "GisKort", "NovaDagsordenspunkt"]
     description: str
     approved: bool
-    document_date: str
-    file_extension: str
+    document_number: Optional[str] = None
+    document_date: Optional[str] = None
+    file_extension: Optional[str] = None
 
 
 @dataclass(frozen=True, slots=True, kw_only=True)
@@ -41,6 +43,21 @@ class JournalNote:
 
 
 @dataclass(frozen=True, slots=True, kw_only=True)
+class Task:
+    """A dataclass representing a KMD Nova task."""
+    uuid: str
+    title: str
+    description: Optional[str]
+    case_worker_ident: Optional[str]
+    case_worker_uuid: str
+    status_code: Literal['F', 'N', 'S']  # Finished, Not Started, Started
+    deadline: datetime
+    created_date: Optional[datetime]
+    started_date: Optional[datetime]
+    closed_date: Optional[datetime]
+
+
+@dataclass(frozen=True, slots=True, kw_only=True)
 class NovaCase:
     """A dataclass representing a KMD Nova case."""
     uuid: str
@@ -52,3 +69,4 @@ class NovaCase:
     case_parties: list[CaseParty]
     document_count: int
     note_count: int
+    kle_number: str
