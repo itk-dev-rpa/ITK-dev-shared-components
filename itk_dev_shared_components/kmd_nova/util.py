ae83f6a2c4602be5da761334b2828ed77c3c5bee
Mathias G <ghbm@aarhus.dk>
2024-02-05 13:11:37 +0100

    Added datetime to iso string function

--- itk_dev_shared_components/kmd_nova/util.py
+++ itk_dev_shared_components/kmd_nova/util.py
@@ -18,3 +18,19 @@ def datetime_from_iso_string(date_string: Optional[str]) -> Optional[datetime]:
         return None
 
     return datetime.fromisoformat(date_string)
+
+
+def datetime_to_iso_string(datetime_: Optional[datetime]) -> Optional[str]:
+    """A helper function to convert a datetime to an ISO formatted string.
+    If the the datetime is None, None is returned.
+
+    Args:
+        datetime_: The datetime object to convert.
+
+    Returns:
+        A datetime string in ISO format.
+    """
+    if datetime_:
+        return datetime_.isoformat()
+
+    return None
