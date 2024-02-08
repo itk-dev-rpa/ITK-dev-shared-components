0618ecdf3acf14b411f59aa676332d7d8ef9e4d6
Mathias G <ghbm@aarhus.dk>
2024-02-08 12:44:22 +0100

    Url parameters moved to dict

--- itk_dev_shared_components/kmd_nova/cpr.py
+++ itk_dev_shared_components/kmd_nova/cpr.py
@@ -21,10 +21,15 @@ def get_address_by_cpr(cpr: str, nova_access: NovaAccess) -> dict:
         requests.exceptions.HTTPError: If the request failed.
     """
 
-    url = f"{nova_access.domain}/api/Cpr/GetAddressByCpr?TransactionId={uuid.uuid4()}&Cpr={cpr}&api-version=1.0-Cpr"
+    url = f"{nova_access.domain}/api/Cpr/GetAddressByCpr"
+    params = {
+        "TransactionId": str(uuid.uuid4()),
+        "Cpr": cpr,
+        "api-version": "1.0-Cpr"
+    }
     headers = {'Authorization': f"Bearer {nova_access.get_bearer_token()}"}
 
-    response = requests.get(url, headers=headers, timeout=60)
+    response = requests.get(url, params=params, headers=headers, timeout=60)
     response.raise_for_status()
     address = response.json()
     return address
