c34180654fa3ed5253173d8a5af1a9d63dc166f9
Mathias G <ghbm@aarhus.dk>
2024-02-19 12:51:55 +0100

    Removed unused functions

--- itk_dev_shared_components/sap/sap_login.py
+++ itk_dev_shared_components/sap/sap_login.py
@@ -2,65 +2,13 @@
 as well as a function to change user passwords."""
 
 import os
-import pathlib
 import subprocess
 import time
 
-from selenium import webdriver
-from selenium.webdriver.common.by import By
 import pywintypes
 import win32com.client
 
 from itk_dev_shared_components.sap import multi_session
-
-
-def login_using_portal(username: str, password: str):
-    """Open KMD Portal in Edge, login and start SAP GUI.
-    Args:
-        user: KMD Portal username.
-        password: KMD Portal password.
-    """
-    driver = webdriver.Chrome()
-    driver.implicitly_wait(10)
-    driver.get('https://portal.kmd.dk/irj/portal')
-    driver.maximize_window()
-
-    # Login
-    user_field = driver.find_element(By.ID, 'logonuidfield')
-    pass_field = driver.find_element(By.ID, 'logonpassfield')
-    login_button = driver.find_element(By.ID, 'buttonLogon')
-    user_field.clear()
-    user_field.send_keys(username)
-    pass_field.clear()
-    pass_field.send_keys(password)
-    login_button.click()
-
-    # Opus
-    mine_genveje = driver.find_element(By.CSS_SELECTOR, "div[title='Mine Genveje']")
-    mine_genveje.click()
-
-    # Wait for download and launch file
-    _wait_for_download()
-
-    driver.quit()
-    _wait_for_sap_session(10)
-
-
-def _wait_for_download():
-    """Private function that checks if the SAP.erp file has been downloaded.
-    Raises:
-        TimeoutError: If the file hasn't been downloaded within 5 seconds.
-    """
-    downloads_folder = str(pathlib.Path.home() / "Downloads")
-    for _ in range(10):
-        for file in os.listdir(downloads_folder):
-            if file.endswith(".sap"):
-                path = os.path.join(downloads_folder, file)
-                os.startfile(path)
-                return
-
-        time.sleep(0.5)
-    raise TimeoutError(f".sap file not found in {downloads_folder}")
 
 
 def login_using_cli(username: str, password: str, client: str = '751', system: str = 'P02', timeout: int = 10) -> None:
