42080770cc88db947e5aa6fd61585bb2fc34ca66
Mathias G <ghbm@aarhus.dk>
2024-03-25 09:57:41 +0100

    Removed actual email addresses

--- tests/test_smtp/test_smtp_util.py
+++ tests/test_smtp/test_smtp_util.py
@@ -11,6 +11,7 @@ from itk_dev_shared_components.smtp.smtp_util import EmailAttachment
 
 class TestTreeUtil(unittest.TestCase):
     """Tests relating to the module smtp.smtp_util."""
+    smtp_server = "localhost"
     smtp_port = 1025
     http_port = 8025
 
@@ -21,17 +22,17 @@ class TestTreeUtil(unittest.TestCase):
 
     def test_send_simple(self):
         """Test sending a simple email."""
-        smtp_util.send_email("itk-rpa@mkb.aarhus.dk", "idsc@test.dk", "Test simple", "Test body", smtp_server="localhost", smtp_port=self.smtp_port)
+        smtp_util.send_email("test@receiver.com", "idsc@test.dk", "Test simple", "Test body", smtp_server=self.smtp_server, smtp_port=self.smtp_port)
 
         message = self.get_message("Test simple")
         self.assertEqual(message["From"]["Address"], "idsc@test.dk")
-        self.assertEqual(message["To"][0]["Address"], "itk-rpa@mkb.aarhus.dk")
+        self.assertEqual(message["To"][0]["Address"], "test@receiver.com")
         self.assertEqual(message["Snippet"], "Test body")
 
     def test_send_html(self):
         """Test sending an html email."""
         html = "<html><head><style> table {font-family: arial, sans-serif; border-collapse: collapse; width: 100%;} td, th { border: 1px solid #dddddd; text-align: left; padding: 8px;}</style></head><body><h2>HTML Table</h2><table><tr><th>Company</th><th>Contact</th><th>Country</th></tr><tr><td>Alfreds Futterkiste</td><td>Maria Anders</td><td>Germany</td></tr><tr><td>Centro comercial Moctezuma</td><td>Francisco Chang</td><td>Mexico</td></tr></table></body></html>"
-        smtp_util.send_email("itk-rpa@mkb.aarhus.dk", "idsc@test.dk", "Test html", html, html_body=True, smtp_server="localhost", smtp_port=self.smtp_port)
+        smtp_util.send_email("test@receiver.com", "idsc@test.dk", "Test html", html, html_body=True, smtp_server=self.smtp_server, smtp_port=self.smtp_port)
 
         message = self.get_message("Test html")
         self.assertTrue(message["Snippet"].startswith("HTML Table"))
@@ -49,14 +50,14 @@ class TestTreeUtil(unittest.TestCase):
                 EmailAttachment(file, file_name)
             )
 
-        smtp_util.send_email("itk-rpa@mkb.aarhus.dk", "idsc@test.dk", "Test files", "This email has three attached txt files.", attachments=attachments, smtp_server="localhost", smtp_port=self.smtp_port)
+        smtp_util.send_email("test@receiver.com", "idsc@test.dk", "Test files", "This email has three attached txt files.", attachments=attachments, smtp_server=self.smtp_server, smtp_port=self.smtp_port)
 
         message = self.get_message("Test files")
         self.assertEqual(message["Attachments"], 3)
 
     def test_send_multiple(self):
         """Test sending to multiple receivers."""
-        smtp_util.send_email(["itk-rpa@mkb.aarhus.dk", "itk-rpa@mkb.aarhus.dk"], "idsc@test.dk", "Test multiple", "This email has multiple receivers.", smtp_server="localhost", smtp_port=self.smtp_port)
+        smtp_util.send_email(["test@receiver.com", "test@receiver.com"], "idsc@test.dk", "Test multiple", "This email has multiple receivers.", smtp_server=self.smtp_server, smtp_port=self.smtp_port)
 
         message = self.get_message("Test multiple")
         self.assertEqual(len(message["To"]), 2)
