e9c948866240b2dc712f646a3e57719e3181a274
Mathias G <ghbm@aarhus.dk>
2024-03-25 09:54:34 +0100

    Added assertions to tests

--- tests/test_smtp/test_smtp_util.py
+++ tests/test_smtp/test_smtp_util.py
@@ -3,21 +3,38 @@
 import unittest
 from io import BytesIO
 
+import requests
+
 from itk_dev_shared_components.smtp import smtp_util
 from itk_dev_shared_components.smtp.smtp_util import EmailAttachment
 
 
 class TestTreeUtil(unittest.TestCase):
     """Tests relating to the module smtp.smtp_util."""
+    smtp_port = 1025
+    http_port = 8025
+
+    def setUp(self) -> None:
+        url = url = f"http://localhost:{self.http_port}/api/v1/messages"
+        response = requests.delete(url, timeout=2)
+        self.assertEqual(response.status_code, 200)
 
     def test_send_simple(self):
         """Test sending a simple email."""
-        smtp_util.send_email("itk-rpa@mkb.aarhus.dk", "idsc@test.dk", "Test simple", "Test body")
+        smtp_util.send_email("itk-rpa@mkb.aarhus.dk", "idsc@test.dk", "Test simple", "Test body", smtp_server="localhost", smtp_port=self.smtp_port)
 
-    def test_send_html_email(self):
+        message = self.get_message("Test simple")
+        self.assertEqual(message["From"]["Address"], "idsc@test.dk")
+        self.assertEqual(message["To"][0]["Address"], "itk-rpa@mkb.aarhus.dk")
+        self.assertEqual(message["Snippet"], "Test body")
+
+    def test_send_html(self):
         """Test sending an html email."""
         html = "<html><head><style> table {font-family: arial, sans-serif; border-collapse: collapse; width: 100%;} td, th { border: 1px solid #dddddd; text-align: left; padding: 8px;}</style></head><body><h2>HTML Table</h2><table><tr><th>Company</th><th>Contact</th><th>Country</th></tr><tr><td>Alfreds Futterkiste</td><td>Maria Anders</td><td>Germany</td></tr><tr><td>Centro comercial Moctezuma</td><td>Francisco Chang</td><td>Mexico</td></tr></table></body></html>"
-        smtp_util.send_email("itk-rpa@mkb.aarhus.dk", "idsc@test.dk", "Test html", html, html_body=True)
+        smtp_util.send_email("itk-rpa@mkb.aarhus.dk", "idsc@test.dk", "Test html", html, html_body=True, smtp_server="localhost", smtp_port=self.smtp_port)
+
+        message = self.get_message("Test html")
+        self.assertTrue(message["Snippet"].startswith("HTML Table"))
 
     def test_send_attachments(self):
         """Test sending an email with multiple attachments."""
@@ -32,11 +49,39 @@ class TestTreeUtil(unittest.TestCase):
                 EmailAttachment(file, file_name)
             )
 
-        smtp_util.send_email("itk-rpa@mkb.aarhus.dk", "idsc@test.dk", "Test files", "This email has three attached txt files.", attachments=attachments)
+        smtp_util.send_email("itk-rpa@mkb.aarhus.dk", "idsc@test.dk", "Test files", "This email has three attached txt files.", attachments=attachments, smtp_server="localhost", smtp_port=self.smtp_port)
+
+        message = self.get_message("Test files")
+        self.assertEqual(message["Attachments"], 3)
 
     def test_send_multiple(self):
         """Test sending to multiple receivers."""
-        smtp_util.send_email(["itk-rpa@mkb.aarhus.dk", "itk-rpa@mkb.aarhus.dk"], "idsc@test.dk", "Test multiple", "This email has multiple receivers.")
+        smtp_util.send_email(["itk-rpa@mkb.aarhus.dk", "itk-rpa@mkb.aarhus.dk"], "idsc@test.dk", "Test multiple", "This email has multiple receivers.", smtp_server="localhost", smtp_port=self.smtp_port)
+
+        message = self.get_message("Test multiple")
+        self.assertEqual(len(message["To"]), 2)
+
+    def get_message(self, subject: str):
+        """Get a message from the Mailpit api with the given subject.
+        https://mailpit.axllent.org/docs/api-v1/view.html#tag--messages
+
+        Args:
+            subject: The email subject to search for.
+
+        Returns:
+            A dict representing the message from Mailpit.
+        """
+        url = f"http://localhost:{self.http_port}/api/v1/messages"
+        response = requests.get(url, timeout=2).json()
+
+        message = None
+        for msg in response["messages"]:
+            if msg["Subject"] == subject:
+                message = msg
+                break
+
+        self.assertIsNotNone(message)
+        return message
 
 
 if __name__ == '__main__':
