d618d91e1a9bd934b5af112b9557644d5fea4ed4
Mathias G <ghbm@aarhus.dk>
2024-08-06 08:40:30 +0200

    Added cvr_mass_lookup

--- itk_dev_shared_components/cvr_api/cvr_lookup.py
+++ itk_dev_shared_components/cvr_api/cvr_lookup.py
@@ -61,15 +61,62 @@ def cvr_lookup(cvr: str, username: str, password: str) -> Company:
     if response_json['hits']['total'] > 1:
         raise ValueError("More than one company found on the given cvr.")
 
-    cvr_result = str(response_json['hits']['hits'][0]['_source']['Vrvirksomhed']['cvrNummer'])
-    if cvr_result != cvr:
-        raise RuntimeError("The resulting cvr number didn't match the searched cvr number.")
-
-    company_dict = response_json['hits']['hits'][0]['_source']['Vrvirksomhed']['virksomhedMetadata']
-    return _unpack_company_dict(company_dict, cvr_result)
+    company_dict = response_json['hits']['hits'][0]['_source']['Vrvirksomhed']
+    return _unpack_company_dict(company_dict)
 
 
-def _unpack_company_dict(company_dict: dict, cvr: str) -> Company:
+def cvr_mass_lookup(cvr_list: list[str], match_length: bool, username: str, password: str) -> list[Company]:
+    """Look up multiple companies based on a list of cvr numbers.
+
+    Args:
+        cvr_list: The list of cvr numbers to look up.
+        match_length: Whether the number results must match the number of inputs.
+        username: The username to the cvr api.
+        password: The password to the cvr api.
+
+    Raises:
+        ValueError: If match_length is true and the number of results doesn't match.
+
+    Returns:
+        A list of company objects describing the found companies.
+        The list is sorted to match the input list.
+    """
+    url = "http://distribution.virk.dk/cvr-permanent/virksomhed/_search"
+    headers = {
+        "Content-Type": "application/json"
+    }
+    auth = HTTPBasicAuth(username, password)
+
+    data = {
+        "_source": ["Vrvirksomhed.virksomhedMetadata", "Vrvirksomhed.cvrNummer"],
+        "query": {
+            "terms": {
+                "Vrvirksomhed.cvrNummer": cvr_list
+            }
+        }
+    }
+
+    response = requests.post(url, headers=headers, auth=auth, json=data, timeout=5)
+    response.raise_for_status()
+
+    response_json = response.json()
+
+    if match_length and response_json['hits']['total'] != len(cvr_list):
+        raise ValueError(f"The number of companies found didn't match the input list: {len(cvr_list)} != {response_json['hits']['total']}")
+
+    companies = []
+
+    for hit in response_json['hits']['hits']:
+        company_dict = hit['_source']['Vrvirksomhed']
+        companies.append(_unpack_company_dict(company_dict))
+
+    # Sort to match input list
+    companies.sort(key=lambda c: cvr_list.index(c.cvr))
+
+    return companies
+
+
+def _unpack_company_dict(company_dict: dict) -> Company:
     """Unpack a response dict to a company object.
 
     Args:
@@ -79,7 +126,10 @@ def _unpack_company_dict(company_dict: dict, cvr: str) -> Company:
     Returns:
         A company object describing the found company.
     """
+    cvr = str(company_dict['cvrNummer'])
+    company_dict = company_dict['virksomhedMetadata']
     address, postal_code, city = _parse_address(company_dict['nyesteBeliggenhedsadresse'])
+
     return Company(
         cvr=cvr,
         name=company_dict['nyesteNavn']['navn'],
