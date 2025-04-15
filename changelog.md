686a6f014ddec1b52134e63d7692fbaabc6c07d1
kri-bak <kriba@aarhus.dk>
2025-04-15 14:19:04 +0200

    Eflyt case search now handles list of cases without a deadline

--- changelog.md
+++ changelog.md
@@ -7,6 +7,12 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [2.11.1] - 2025-04-15
+
+### Fixed
+
+- Eflyt case search now handles list of cases without a deadline
+
 ## [2.10.0] - 2025-03-20
 
 ### Changed
--- itk_dev_shared_components/eflyt/eflyt_search.py
+++ itk_dev_shared_components/eflyt/eflyt_search.py
@@ -5,6 +5,7 @@ from typing import Literal
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
+from selenium.common.exceptions import NoSuchElementException
 
 from itk_dev_shared_components.eflyt.eflyt_util import format_date
 from itk_dev_shared_components.eflyt.eflyt_case import Case
@@ -62,26 +63,29 @@ def extract_cases(browser: webdriver.Chrome) -> list[Case]:
     rows.pop(0)
     cases = []
     for row in rows:
-        deadline = row.find_element(By.XPATH, "td[3]/a").text
+        deadline = None
+        start_column = 3
+        try:
+            deadline = row.find_element(By.XPATH, f"td[{start_column}]/a").text
+        except NoSuchElementException:
+            start_column = 2
 
         # Convert deadline to date object
-        if deadline:
+        if deadline is not None:
             deadline = datetime.strptime(deadline, "%d-%m-%Y")
-        else:
-            deadline = None
 
-        case_number = row.find_element(By.XPATH, "td[4]").text
-        case_types_text = row.find_element(By.XPATH, "td[5]").text
+        case_number = row.find_element(By.XPATH, f"td[{start_column + 1}]").text
+        case_types_text = row.find_element(By.XPATH, f"td[{start_column + 2}]").text
 
         # If the case types ends with '...' we need to get the title instead
         if case_types_text.endswith("..."):
-            case_types_text = row.find_element(By.XPATH, "td[5]").get_attribute("Title")
+            case_types_text = row.find_element(By.XPATH, f"td[{start_column + 2}]").get_attribute("Title")
         case_types = case_types_text.split(", ")
 
-        status = row.find_element(By.XPATH, "td[6]").text
-        cpr = row.find_element(By.XPATH, "td[7]/a").text
-        name = row.find_element(By.XPATH, "td[8]").text
-        case_worker = row.find_element(By.XPATH, "td[10]").text
+        status = row.find_element(By.XPATH, f"td[{start_column + 3}]").text
+        cpr = row.find_element(By.XPATH, f"td[{start_column + 4}]/a").text
+        name = row.find_element(By.XPATH, f"td[{start_column + 5}]").text
+        case_worker = row.find_element(By.XPATH, f"td[{start_column + 7}]").text
 
         case = Case(case_number, deadline, case_types, status, cpr, name, case_worker)
         cases.append(case)
