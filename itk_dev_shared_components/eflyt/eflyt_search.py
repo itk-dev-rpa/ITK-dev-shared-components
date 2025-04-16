94a7860debdff0350d40d57d6cbe25c25369aaa2
kri-bak <kriba@aarhus.dk>
2025-04-16 09:44:54 +0200

    Made row index a check on headlines and added deadline to tests

--- itk_dev_shared_components/eflyt/eflyt_search.py
+++ itk_dev_shared_components/eflyt/eflyt_search.py
@@ -5,7 +5,6 @@ from typing import Literal
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
-from selenium.common.exceptions import NoSuchElementException
 
 from itk_dev_shared_components.eflyt.eflyt_util import format_date
 from itk_dev_shared_components.eflyt.eflyt_case import Case
@@ -59,33 +58,31 @@ def extract_cases(browser: webdriver.Chrome) -> list[Case]:
     """
     table = browser.find_element(By.ID, "ctl00_ContentPlaceHolder2_GridViewSearchResult")
     rows = table.find_elements(By.TAG_NAME, "tr")
+    headlines = rows[0].text.split(" ")
+
     # Remove header row
     rows.pop(0)
     cases = []
     for row in rows:
         deadline = None
-        start_column = 3
-        try:
-            deadline = row.find_element(By.XPATH, f"td[{start_column}]/a").text
-        except NoSuchElementException:
-            start_column = 2
+        if "Deadline" in headlines:
+            deadline_text = row.find_element(By.XPATH, f"td[{headlines.index("Deadline") + 1}]/a").text
+            # Convert deadline to date object
+            if len(deadline_text) > 0:
+                deadline = datetime.strptime(deadline_text, "%d-%m-%Y")
 
-        # Convert deadline to date object
-        if deadline is not None:
-            deadline = datetime.strptime(deadline, "%d-%m-%Y")
-
-        case_number = row.find_element(By.XPATH, f"td[{start_column + 1}]").text
-        case_types_text = row.find_element(By.XPATH, f"td[{start_column + 2}]").text
+        case_number = row.find_element(By.XPATH, f"td[{headlines.index("Sagsnr.") + 1}]").text
+        case_types_text = row.find_element(By.XPATH, f"td[{headlines.index("Flyttetype") + 1}]").text
 
         # If the case types ends with '...' we need to get the title instead
         if case_types_text.endswith("..."):
-            case_types_text = row.find_element(By.XPATH, f"td[{start_column + 2}]").get_attribute("Title")
+            case_types_text = row.find_element(By.XPATH, f"td[{headlines.index("Flyttetype") + 1}]").get_attribute("Title")
         case_types = case_types_text.split(", ")
 
-        status = row.find_element(By.XPATH, f"td[{start_column + 3}]").text
-        cpr = row.find_element(By.XPATH, f"td[{start_column + 4}]/a").text
-        name = row.find_element(By.XPATH, f"td[{start_column + 5}]").text
-        case_worker = row.find_element(By.XPATH, f"td[{start_column + 7}]").text
+        status = row.find_element(By.XPATH, f"td[{headlines.index("Status") + 1}]").text
+        cpr = row.find_element(By.XPATH, f"td[{headlines.index("CPR-nr.") + 1}]/a").text
+        name = row.find_element(By.XPATH, f"td[{headlines.index("Navn") + 1}]").text
+        case_worker = row.find_element(By.XPATH, f"td[{headlines.index("Sagsbehandler") + 2}]").text  # eFlyt has an additional empty column before "Sagsbehandler"
 
         case = Case(case_number, deadline, case_types, status, cpr, name, case_worker)
         cases.append(case)
--- tests/test_eflyt/test_search.py
+++ tests/test_eflyt/test_search.py
@@ -40,6 +40,24 @@ class SearchTest(unittest.TestCase):
             self.assertIsInstance(case.case_worker, str)
             self.assertGreater(len(case.case_worker), 0)
 
+    def test_extract_cases_deadline(self):
+        """Extract cases and check we found what we expected"""
+        eflyt_search.search(self.browser, date.today() - timedelta(days=2), date.today(), case_status="Godkendt")
+        cases = eflyt_search.extract_cases(self.browser)
+
+        self.assertGreater(len(cases), 0)
+        for case in cases:
+            self.assertIsInstance(case.case_number, str)
+            self.assertIsInstance(case.case_types, list)
+            self.assertIsInstance(case.deadline, (date, type(None)))
+            self.assertIsInstance(case.status, str)
+            self.assertIsInstance(case.cpr, str)
+            self.assertRegex(case.cpr, r"\d{6}-\d{4}")
+            self.assertIsInstance(case.name, str)
+            self.assertGreater(len(case.name), 0)
+            self.assertIsInstance(case.case_worker, str)
+            self.assertGreater(len(case.case_worker), 0)
+
     def test_open_case(self):
         """Open a case and check the browser opened the case view"""
         test_case = os.getenv("TEST_CASE")
