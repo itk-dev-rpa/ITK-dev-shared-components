4f651ea1589dda32bab10fe070ca2b8e1615d669
Mathias G <ghbm@aarhus.dk>
2026-02-20 11:30:24 +0100

    Ignore lint errors

--- itk_dev_shared_components/eflyt/eflyt_login.py
+++ itk_dev_shared_components/eflyt/eflyt_login.py
@@ -6,8 +6,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webelement import WebElement
 from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException
 
-
-class ResilientBrowser(webdriver.Chrome):  # pylint: disable=inherit-non-class
+# pylint: disable=inherit-non-class, missing-function-docstring, too-few-public-methods
+class ResilientBrowser(webdriver.Chrome):
     """A webdriver.Chrome subclass that retries find_element if the element goes stale."""
     def find_element(self, by=By.ID, value = None) -> WebElement:
         element = super().find_element(by, value)
